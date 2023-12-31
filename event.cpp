2ffea3a0743286410bb4b8067a5a63968c37f3f0
Vaishnovi76ana <106680616+Vaishnovi76ana@users.noreply.github.com>
2023-12-31 14:56:37 +0530

    Update event.cpp. Function names changed so that they are uniform . register function modified.(register is a keyword, cannot be used as a function name)

--- event.cpp
+++ event.cpp
@@ -1,44 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#include "student.h"
-#include "student.cpp"
+#include "event.h"
 
-class event{
-    public:
-    string name,time,venue;
-    string description;
-    list<student> registered;
-    Coordinator cd;
-    event(string name,string time,string venue,string description)
-    {
-        this->name=name;
-        this->time=time;
-        this->venue=venue;
-        this->description=description;
-    }
-    void coordinator_assigned(Coordinator c){
-        cd=c;
-    }
-    void register(student new_entry){
-        registered.pushback(new_entry);
-    }
-    void getEventDetails(){
-        cout<<"Event details:"<<endl;
-        cout<<"name: "<<this->name<<endl;
-        cout<<"time: "<<this->time<<endl;
-        cout<<"venue: "<<this->venue<<endl;
-        cout<<"description: "<<this->description<<endl;
-        cout << "*********************************" <<endl;
-    }
-    void disp_reg(){
-        cout<<"list of registered students :\n";
-        for(auto i:registered)
-            cout<<i->getName()<<" "<<i->getID()<<endl;
-    }
-    void disp_coordinator(){
-        cout<<"coordinator for the event: "<<cd->getName()<<" "<<cd->getID()<<endl;
-    }
-         
-    }
-};
+event::event(string name, string time, string venue, string description, Student event_coordinator)
+{
+    this->name = name;
+    this->time = time;
+    this->venue = venue;
+    this->description = description;
+    this->coordinator_name = event_coordinator.getName();
+    this->coordinator_ID = event_coordinator.getID();
+}
+
+void event :: registerForEvent(Participant new_entry)
+{
+    registered.push_back(new_entry);
+}
+void event :: getEventDetails()
+{
+    cout << "Event details:" << endl;
+    cout << "name: " << this->name << endl;
+    cout << "time: " << this->time << endl;
+    cout << "venue: " << this->venue << endl;
+    cout << "description: " << this->description << endl;
+    cout << "*********************************" << endl;
+}
+void event :: getRegisteredStudents()
+{
+    cout << "list of registered students :\n";
+    for (auto it : registered)
+        cout << it.getName() << " " << it.getID() << endl;
+}
+void event :: getCoordinatorDetails()
+{
+    cout << "coordinator for the event:  Name : " << this->coordinator_name << "\n ";
+    cout << "Roll No : " << this->coordinator_ID << "\n";
+}
+
+string event :: getCoordinatorName()
+{
+    return this->coordinator_name;
+}
+
+string event :: getCoordinatorID()
+{
+    return this->coordinator_ID;
+}
+
+
